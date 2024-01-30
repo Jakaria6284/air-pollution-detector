289015340a55f824da458650e4ec2d1401d551b3
jakaria6284 <kamal.jakaria.6284mia@gmail.com>
2024-01-30 14:44:32 +0600

    first commit

---
+++ app/src/main/java/com/bizlijakaria/cheackairquality/AirviewActivity.java
@@ -0,0 +1,14 @@
+package com.bizlijakaria.cheackairquality;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AirviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airview);
+    }
+}
